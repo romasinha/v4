6c6c277f6c65b7cd7ca2484c47e75c6a31239bd5
romasinha <sinha.roma99@gmail.com>
2022-02-15 05:09:58 +0530

    made changes

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,8 +126,6 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
-
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
       <h2 className="numbered-heading">About Me</h2>
@@ -136,45 +134,39 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! I am Roma Sinha, a junior pursuing a major in Computer Science at Manipal
+              University Jaipur.
+              <br />
+              <br />
+              Interestingly, I have been into Web Development from my freshman year. Soon, I dived
+              into learning advanced Javascript libraries like React and eventually became a
+              MERN-stack developer. I built various projects like <b>NewsNote</b>,{' '}
+              <b>AI-Verifica</b> and <b>Krypto</b>.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I won NPSIHacks'21 where my project- <b>AI-Verifica</b> was adjudged the best among
+              hundreds.
+              <br />I currently am contributing to <b>Moja Global and CircuitVerse.</b>
+            </p>
+            <p>
+              I am also <b>3-star</b> coder at Codechef with my <b>highest rating at 1625</b>. I was
+              also <b>ranked 1938</b> from a total of 40000 in the May Long Challenge’21.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I lead the <b>IEEE-Women in Engineering</b> Society at my college. It started from 20
+              to more than 220 women now. I single-handedly built a website to encourage girls to
+              share resources and collaborate over projects.
             </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
-
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
         </StyledText>
 
         <StyledPic>
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/roma.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
